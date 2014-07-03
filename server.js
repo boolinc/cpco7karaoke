c3c023450a2c16b51e1fa19d7e0bb165cccdcb0a
Pablo Dorado <pandres95@boolinc.co>
2014-07-03 03:26:10 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,41 @@
+// Setup basic express server
+var express = require('express');
+var app =  express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var redis = require('redis').createClient();
+
+
+io.on('connection', function(socket){
+
+    //Init
+    console.log('Conexión detectada');
+    redis.lrange('SONGS', 0, -1, function(err, records){
+        if(!err){
+            records.forEach(function(record){
+                record = JSON.parse(record);
+                socket.emit('message', record);
+            });
+        }
+    });
+
+    //Recibe the request pin
+    socket.on('message', function(twitter, song){
+        console.log("[@%s] %s", twitter, song);
+        record = { twitter: twitter, song: song };
+        redis.lpush('SONGS', JSON.stringify(record));
+        io.emit('message', record);
+    });
+
+    //Disconect
+    socket.on('disconnect', function(){
+        console.log('Desconexión detectada');
+    });
+});
+
+// Routing
+app.use(express.static(__dirname + '/public'));
+
+server.listen(3000, function(){
+    console.log('Server listen on port 3000*');
+})
